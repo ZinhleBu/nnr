6350e7eec7a37d0b1874ed58da7b1d894caa2253
_Zinhle <zbuhlungu1@gmail.com>
2022-02-28 11:36:13 +0200

    layout key

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,11 +3,11 @@ import Footer from '../Footer'
 import Header from '../Header'
 import styles from '../../styles/Home.module.scss'
 
-const Layout = ({ children }) => {
+const Layout = ({ children, index }) => {
   return (
       <>
       <Header/>
-      <div className={styles.main}>{children}</div>
+      <div className={styles.main} key={index}>{children}</div>
       <Footer/>
       </>
   )
