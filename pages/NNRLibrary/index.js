63529b080cf8e59d65162af7a8ee0d184d6f724a
_Zinhle <zbuhlungu1@gmail.com>
2022-03-01 14:53:51 +0200

    removed unused link

--- pages/NNRLibrary/index.js
+++ pages/NNRLibrary/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
 
 export default function NNRLibrary () {
   return (
     <>
     <div>
       <nav>
-        <ul>
-          <li>
-            <Link>
-            <a href="/South_African_Nuclear_Safety_Conventions_Reports">
-            South African Nuclear Safety Conventions Reports
-            </a>
-            </Link>
-          </li>
-          <li>
-            <Link>
-            <a href="/AnnualReprts">
-            South African Nuclear Safety Conventions Reports
-            </a>
-            </Link>
-          </li>
-        </ul>
+        
       </nav>
     </div>
     </>
